8aa47285dbce7aafd4dfc4ee01eaafd05e33683c
sumitraj-saroj <sumitrajsaroj01@gmail.com>
2025-12-09 16:01:57 +0530

    add chapter 05 code

---
+++ Chapter_05 - Diving in to NodeJS github repo/code/modules.js
@@ -0,0 +1,8 @@
+//require("/path");
+// Alll the code of the module is wrapped inside the function(IIFE)
+//IIFE - Imediately Invoked Function Expression
+
+// (function () {
+//     //All the code of the module is runs here
+
+// })();
