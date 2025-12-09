c92b267426606b64dfa501064161f26d044aea64
sumitraj-saroj <sumitrajsaroj01@gmail.com>
2025-12-09 16:17:30 +0530

    add code of chapter 03

---
+++ Chapter_03 - Let's Write the code/code/index.js
@@ -0,0 +1,14 @@
+const name = "sumit";
+var a = 1230;
+var b = 23424;
+var c = a + b;
+
+console.log(name);
+console.log(c);
+
+// console.log(global)
+// globalThis represents the global context, effectively serving as the global object.
+
+
+console.log(globalThis)
+console.log(globalThis=== global)
